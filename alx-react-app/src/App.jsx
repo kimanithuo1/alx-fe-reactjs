f47f4deb7dbcdc60205dfaf8f641dfcb9c372238
kimanithuo1 <jkimanithuo@gmail.com>
2024-11-01 18:00:31 +0300

    added WelcomeMessage.jsx file and exported to App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import WelcomeMessage from './WelcomeMessage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,6 +20,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <WelcomeMessage /> {/* Add WelcomeMessage component here */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
