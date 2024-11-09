5629bcc15c15e428f833ec13f4cea5d2da48f8c0
kimanithuo1 <jkimanithuo@gmail.com>
2024-11-09 23:38:19 +0300

    applied UserContext to import data from UserContext.js in UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,18 @@
-// src/ProfilePage.jsx
-import React from 'react';
-import UserInfo from './UserInfo';
+// src/components/UserProfile.jsx
+import React, { useContext } from 'react';
+import UserContext from '../UserContext'; // Import the UserContext
 
-function ProfilePage() {
-  return <UserInfo />;
+function UserProfile() {
+  // Use useContext to consume the context and get userData
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default ProfilePage;
+export default UserProfile;
