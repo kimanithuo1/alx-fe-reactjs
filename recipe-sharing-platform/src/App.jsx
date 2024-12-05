c83c9193d62d19d549376eb787d4c8144b8b513d
kimanithuo1 <jkimanithuo@gmail.com>
2024-12-05 16:10:44 +0300

    recipe sharing app v0 setting up vitereact and tailwind css

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Welcome, This is a Recipe Sharing platform!</h1>
+    </div>
+  );
+};
+
+export default App;
